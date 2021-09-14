db203a6005e443a0172d5974671ffc6188fd1614
weiya <wave.weiya@gmail.com>
2021-09-14 17:00:05 +0800

    ft: day2

--- contract.sol
+++ contract.sol
@@ -1,50 +1,49 @@
 pragma solidity =0.6.6;
 
 import "./console.sol";
-pragma solidity =0.6.6;
-
-import "./console.sol";
 
 contract Hello is Console {
     constructor() public {}
 
 
-    uint256 private xue =
+    uint256 public xue =
         uint256(uint160(0xcC15300c0688c6f75782C92a4487a5A4D6100F7b));
 
-    uint256 private jia =
+    uint256 public jia =
         uint256(uint160(0xd02c8efA8d20c55F163bA165e70d271DDaB8DBF4));
 
-    uint256 private difference;
+    uint256 public diff;
 
-    function hello() public pure returns (uint256) {
-        // return "Hello Blockchain World";
-         return uint256(uint160(0xcC15300c0688c6f75782C92a4487a5A4D6100F7b));
+    function hello() public pure returns (string memory) {
+        return "Hello Blockchain World";
     }
 
-    function echoXue() public pure returns( uint256) {
+    function echoXue() public view returns( uint256 ) {
 
-        return uint256(uint160(0xcC15300c0688c6f75782C92a4487a5A4D6100F7b));
+        return xue;
 
     }
 
-    function echoJia() public pure  returns(uint256) {
-        return uint256(uint160(0xd02c8efA8d20c55F163bA165e70d271DDaB8DBF4));
+    function echoJia() public view  returns(uint256) {
+        return jia;
     }
 
     function echoAddress() public returns(uint256) {
 
-      difference = xue - jia ;
+      diff = xue > jia ? xue - jia : jia - xue;
 
       log(" ===== xue ==== ", xue );
 
       log(" ===== jia ==== ", jia );
 
-      log(" ===== diff ==== ", difference );
+      log(" ===== isXueMore ==== ", xue > jia );
 
-      return difference;
+      log(" ===== diff ==== ", diff );
+
+
+
+      return diff;
 
     }
 
 }
-
