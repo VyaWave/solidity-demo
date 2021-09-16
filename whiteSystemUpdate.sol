28bcfe0f3cd53d4fa4ea5176bb96720c544fe594
weiya <wave.weiya@gmail.com>
2021-09-16 11:34:27 +0800

    day2

--- whiteSystemUpdate.sol
+++ whiteSystemUpdate.sol
@@ -1,125 +1,88 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity = 0.6.6;
 
-contract WhiteSystem {
-
-  uint160[] public whiteList;
-
-  uint160[] public validWhitelist;
-
-  uint256 MAX_LIMIT = 100;
+contract WhiteList {
 
 
-  // 设置最大数量
-  function setMaxLimt( uint256 max ) public {
+    uint MAX_LIMIT = 100;
+    mapping(address => uint) temporary;
+    address[] private whiteList;
 
-    MAX_LIMIT = max;
-
-  }
-
-  // 检查上限
-  function checkLimit() public view returns( bool limit ) {
-
-    uint length = whiteList.length -1;
-
-    uint validCount = 0;
-
-
-    for( uint i = 0; i < length; i ++ ) {
-      if ( uint256( uint160( whiteList[i] ) ) > 0 ) {
-        validCount++;
-      }
+    function setMaxLimt(uint _maxlimt) public {
+        MAX_LIMIT = _maxlimt;
     }
 
-    limit = validCount >= MAX_LIMIT;
+    function checkInList(address _item) public view returns(bool result) {
 
-  }
-
-
-  // 获取对应的索引值
-  function checkIndex( uint160 item ) public view returns( uint256 targetIndex ){
-
-    targetIndex = 0;
-
-    uint length = whiteList.length -1;
-
-    for( uint i = 0; i < length; i ++ ) {
-      if ( uint256(uint160(item)) == uint256(uint160(whiteList[i])) ) {
-        targetIndex = i;
-        return targetIndex;
-      }
-    }
-  }
-
-  // 获取是否存在
-  function checkin( uint160 item ) public view returns( bool inList ){
-
-    uint length = whiteList.length - 1;
-
-    for( uint i = 0; i < length; i ++ ) {
-      if ( uint256(uint160(item)) == uint256(uint160(whiteList[i])) ) {
-        inList = true;
-        return inList;
-      }
-    }
-  }
-
-  // 增加一个
-  function addOne( uint160 item ) public returns( bool result ) {
-
-    bool inList = checkin(item);
-    bool uptoLimt = checkLimit();
-
-    require( !inList, " white list is upto limit! " );
-    require( !uptoLimt, " Item is allready in list! ");
-
-    whiteList.push( item );
-
-    result = true;
-
-  }
-
-  // 移除一个
-  function removeOne( uint160 item ) public returns( bool result ) {
-
-
-    bool inList = checkin(item);
-
-    require( !inList, " Item is not in list! ");
-
-    uint index = checkIndex(item);
-
-    delete whiteList[index];
-
-    result = true;
-
-  }
-
-  // 获取有效地址
-  function getWhiteList() public {
-
-    uint length = whiteList.length - 1;
-
-    for( uint i = 0; i < length; i ++ ) {
-
-        if ( uint256(uint160(whiteList[i])) > 0 ) {
-          validWhitelist.push(whiteList[i]);
+        if (temporary[_item] > 0 ) {
+            result = true;
+        } else {
+            result = false;
         }
     }
 
-  }
 
-  // 获取有效地址数量
-  function getCount() public view returns( uint count) {
+     function checkMaxLimit() private view returns(bool uptoLimt) {
 
-    uint length = whiteList.length - 1;
-
-    for( uint i = 0; i < length; i ++ ) {
-        if ( uint256(uint160(whiteList[i])) > 0 ) {
-          count ++;
+        if (whiteList.length >= MAX_LIMIT) {
+            uptoLimt = true;
+        } else {
+            uptoLimt = false;
         }
+
     }
 
-  }
+
+    function addOne(address _item) public returns(bool result) {
+
+        require(!checkMaxLimit(), " upto limt! ");
+
+        require(!checkInList(_item), " It is allready in list! ");
+
+        whiteList.push(_item);
+
+        uint length = whiteList.length;
+
+        temporary[ _item ] = length;
+
+        result = true;
+
+    }
+
+    function removeOne(address _item) public returns(bool result) {
+
+        require(checkInList(_item), " It is not in list! ");
+
+        uint index = temporary[_item] - 1;
+
+        uint length = whiteList.length;
+
+        address _temp = whiteList[length -1 ];
+
+        whiteList[length - 1 ] = _item;
+
+        whiteList[index] = _temp;
+
+        temporary[_temp] = index + 1;
+
+        temporary[ _item ] = 0;
+
+        whiteList.pop();
+
+        result = true;
+
+    }
+
+    function viewTotal() public view returns(uint total) {
+
+        total = whiteList.length;
+
+    }
+
+    function viewList() public view returns( address[] memory list ){
+
+        list = whiteList;
+
+    }
 
 }
